545bb284539538c5b698d9a5add2a2c60ddd37fd
Andrew17-2006 <andriy.veretelnyk@gmail.com>
2025-11-13 21:51:30 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// write code here
+const center = document.querySelector('.spider');
+center.style.position = 'absolute';
+center.style.top = '50%';
+center.style.left = '50%';
+center.style.transform = 'translate(-50%, -50%)';
